f5c9fe19f41b98718f7c1a6e20bf45bc9b2518fe
Zoe Aubert <hello@zoeaubert.me>
2022-05-26 20:04:26 +0100

    Vast improvements

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,28 @@
 module.exports = {
   darkMode: "class",
   content: ["./layouts/**/*.html", "./content/**/*.md"],
+  safelist: [
+    "photogrid",
+    "microblog_conversation",
+    "microblog_post",
+    "microblog_avatar",
+    "microblog_time",
+    "microblog_time",
+  ],
   theme: {
     extend: {
       colors: {
         background: {
-          DEFAULT: '#FFFFFF',
+          DEFAULT: "#FFFFFF",
           dark: "#1B1B1B",
         },
         text: {
-          DEFAULT: '#101010',
-          dark: '#F0F0F0',
+          DEFAULT: "#101010",
+          dark: "#F0F0F0",
         },
         secondary: {
-          DEFAULT: '#303030',
-          dark: '#C0C0C0',
+          DEFAULT: "#303030",
+          dark: "#C0C0C0",
         },
         category: {
           DEFAULT: "#E2E1E1",
@@ -27,7 +35,7 @@ module.exports = {
         border: {
           DEFAULT: "#888888",
         },
-        middleGray: '#888888'
+        middleGray: "#888888",
       },
     },
   },
